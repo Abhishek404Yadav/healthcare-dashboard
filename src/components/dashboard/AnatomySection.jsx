50215347998030873259c26427364ac6c307e162
Abhishek Yadav <abhiyadav.ce@gmail.com>
2025-05-25 19:42:08 +0530

    fixing vercel image load issue

--- src/components/dashboard/AnatomySection.jsx
+++ src/components/dashboard/AnatomySection.jsx
@@ -1,11 +1,10 @@
-import humanAnatomy from '../../assets/HumanAna.png';
 const AnatomySection = () => {
   return (
     <div className="bg-[#F6FAFF] rounded-2xl py-10 mx-15 max-w-2xs relative">
       <div className="flex justify-center">
         <div className="relative">
           <img
-            src={humanAnatomy}
+            src="/HumanAna.png"
             alt="Human Body Silhouette"
             className="w-52 h-auto object-contain"
           />
